1913aba5ea6446ee75221fa4a4d78714093fc6dd
Gustaf Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2021-07-26 15:48:36 +0200

    Complete 'Introducing JSX' section of guide

--- react-guide/src/index.js
+++ react-guide/src/index.js
@@ -7,8 +7,31 @@ import './index.css';
 //   document.getElementById('root')
 // );
 
-const name = 'Josh Perez';
-const element = <h1>Hello, {name}</h1>
+// const name = 'Josh Perez';
+// const element = <h1>Hello, {name}</h1>
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger</h1>
+}
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    {getGreeting(user)}
+  </h1>
+);
+
 
 ReactDOM.render(
   element,
